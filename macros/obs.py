49bf51eff65238be78053815e079651064e1a6dd
Eric Mesa <ericsbinaryworld@gmail.com>
2022-01-23 17:28:30 -0500

    updated OBS keys

--- macros/obs.py
+++ macros/obs.py
@@ -1,5 +1,3 @@
-# MACROPAD Hotkeys example: Microsoft Edge web browser for Windows
-
 from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
 
 app = {                      # REQUIRED dict, must be named 'app'
@@ -9,21 +7,20 @@ app = {                      # REQUIRED dict, must be named 'app'
         # 1st row ----------
         (0xFF0000, 'REC', [Keycode.CONTROL, Keycode.ALT, Keycode.R]),
         (0xD4D900, 'PAUSE', [Keycode.CONTROL, Keycode.ALT, Keycode.P]),
-        (0x00D900, 'STREAM', [Keycode.CONTROL, Keycode.ALT, Keycode.S]),      # Scroll up
+        (0x00D900, 'STREAM', [Keycode.CONTROL, Keycode.ALT, Keycode.E]),
         # 2nd row ----------
-        (0x101010, 'Scn 1', [Keycode.CONTROL, Keycode.ALT, '1']),
-        (0x101010, 'Scn 2', [Keycode.CONTROL, Keycode.ALT, '2']),
-        (0x101010, 'Scn 3', [Keycode.CONTROL, Keycode.ALT, '3']),                     # Scroll down
+        (0x101010, 'Pre-', [Keycode.CONTROL, Keycode.ALT, '1']),
+        (0x101010, 'Appeal', [Keycode.CONTROL, Keycode.ALT, '2']),
+        (0x101010, 'Title', [Keycode.CONTROL, Keycode.ALT, '3']),
         # 3rd row ----------
-        (0x101010, 'Scn 4', [Keycode.CONTROL, Keycode.ALT, '4']),
-        (0x101010, 'Scn 5', [Keycode.CONTROL, Keycode.ALT, '5']),
-        (0x101010, 'Scn 6', [Keycode.CONTROL, Keycode.ALT, '6']),
-        # 4th row ---------- (as of 20210801 these are unused for my OBS config)
-        (0x000000, 'Ada', [Keycode.CONTROL, 'n', -Keycode.COMMAND,
-                           'www.adafruit.com\n']),   # Adafruit in new window
-        (0x800000, 'Digi', [Keycode.CONTROL, 'n', -Keycode.COMMAND,
+        (0x101010, 'Me ^', [Keycode.CONTROL, Keycode.ALT, '4']),
+        (0x101010, 'Game ^', [Keycode.CONTROL, Keycode.ALT, '5']),
+        (0x101010, 'Med', [Keycode.CONTROL, Keycode.ALT, '6']),
+        # 4th row ----------
+        (0x000000, 'BRB', [Keycode.CONTROL, Keycode.ALT, '7']),
+        (0x800000, 'none', [Keycode.CONTROL, 'n', -Keycode.COMMAND,
                             'www.digikey.com\n']),   # Digi-Key in new window
-        (0x101010, 'Hacks', [Keycode.CONTROL, 'n', -Keycode.COMMAND,
+        (0x101010, 'none', [Keycode.CONTROL, 'n', -Keycode.COMMAND,
                              'www.hackaday.com\n']), # Hack-a-Day in new win
         # Encoder button ---
         (0x000000, '', [Keycode.CONTROL, 'w']) # Close tab
