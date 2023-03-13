49f17aecac1898ba8922a76ac885dae2b349d4a9
Eric Mesa <ericsbinaryworld@gmail.com>
2023-03-13 16:49:05 -0400

    change labels for plasma

--- macros/plasma.py
+++ macros/plasma.py
@@ -5,21 +5,21 @@ app = {                      # REQUIRED dict, must be named 'app'
     'macros' : [             # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0xFF0000, 'REC', [Keycode.CONTROL, Keycode.ALT, '1']),
-        (0xD4D900, 'PAUSE', [Keycode.CONTROL, Keycode.ALT, '2']),
-        (0x00D900, 'STREAM', [Keycode.CONTROL, Keycode.ALT, '3']),
+        (0xFF0000, 'Audio', [Keycode.CONTROL, Keycode.ALT, '1']),
+        (0xD4D900, 'Books', [Keycode.CONTROL, Keycode.ALT, '2']),
+        (0x00D900, 'Comics', [Keycode.CONTROL, Keycode.ALT, '3']),
         # 2nd row ----------
-        (0x101010, 'Pre-', [Keycode.CONTROL, Keycode.ALT, '4']),
-        (0x101010, 'Appeal', [Keycode.CONTROL, Keycode.ALT, '5']),
-        (0x101010, 'Title', [Keycode.CONTROL, Keycode.ALT, '6']),
+        (0x101010, 'Comms', [Keycode.CONTROL, Keycode.ALT, '4']),
+        (0x101010, 'Main', [Keycode.CONTROL, Keycode.ALT, '5']),
+        (0x101010, 'Photo', [Keycode.CONTROL, Keycode.ALT, '6']),
         # 3rd row ----------
-        (0x101010, 'Me ^', [Keycode.CONTROL, Keycode.ALT, '7']),
-        (0x101010, 'Game ^', [Keycode.CONTROL, Keycode.ALT, '8']),
-        (0x101010, 'Med', [Keycode.CONTROL, Keycode.ALT, '9']),
+        (0x101010, 'Prog', [Keycode.CONTROL, Keycode.ALT, '7']),
+        (0x101010, 'Vid Ed', [Keycode.CONTROL, Keycode.ALT, '8']),
+        (0x101010, 'Games', [Keycode.CONTROL, Keycode.ALT, '9']),
         # 4th row ----------
-        (0x000000, 'BRB', [Keycode.CONTROL, Keycode.ALT, '0']),
-        (0x800000, 'none', [Keycode.CONTROL, Keycode.ALT, '!']), 
-        (0x101010, 'none', [Keycode.CONTROL, Keycode.ALT, '@']), 
+        (0x000000, 'Vids', [Keycode.CONTROL, Keycode.ALT, '0']),
+        (0x800000, 'Virt', [Keycode.CONTROL, Keycode.ALT, '!']), 
+        (0x101010, 'Web', [Keycode.CONTROL, Keycode.ALT, '@']), 
         # Encoder button ---
         (0x000000, '', [Keycode.CONTROL, 'w']) # Close tab
     ]
